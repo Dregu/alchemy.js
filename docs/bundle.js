2426f644f5bbbbf99106609b63cfc6d22181e6c9
Dregu <dregu@dreg.us>
2019-10-15 15:18:47 +0300

    wat?

--- docs/bundle.js
+++ docs/bundle.js
@@ -114,15 +114,15 @@ module.exports = class Alchemy {
 const Alchemy = require('./alchemy.js')
 const alchemy = new Alchemy()
 document.getElementById('seed').onkeyup = function(e) {
-  if(e.keycode = 13) {
-    if(this.value == "") {
-      document.getElementById("recipe").innerHTML = ""
-      return
-    }
-    e.preventDefault()
-    var alc = alchemy.get_alchemy(this.value, 1)
-    document.getElementById("recipe").innerHTML = "LC: "+alc["LC"].join(", ")+"<br/>"+"AP: "+alc["AP"].join(", ")
+  if(this.value == "") {
+    document.getElementById("recipe").innerHTML = ""
+    return
   }
+  e.preventDefault()
+  var seed = parseInt(this.value)
+  if(!seed) return
+  var alc = alchemy.get_alchemy(seed, true)
+  document.getElementById("recipe").innerHTML = "LC: "+alc["LC"].join(", ")+"<br/>"+"AP: "+alc["AP"].join(", ")
 }
 document.getElementById('seed').focus()
 
